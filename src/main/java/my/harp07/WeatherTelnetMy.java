ae72d6d6f24b31ab0aa3efe5e5c7a505da6bcaf3
Roman Koldaev <harp07@mail.ru>
2019-08-19 15:03:43 +0400

    correct

--- src/main/java/my/harp07/WeatherTelnetMy.java
+++ src/main/java/my/harp07/WeatherTelnetMy.java
@@ -4,6 +4,9 @@ package my.harp07;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.net.telnet.TelnetClient;
 
 public final class WeatherTelnetMy {
@@ -18,11 +21,14 @@ public final class WeatherTelnetMy {
             e.printStackTrace();
             System.exit(1);
         }
+        
         IOUtilmy.readWrite(
                 new BufferedInputStream(tc.getInputStream()), 
                 new BufferedOutputStream(tc.getOutputStream()),
                 new BufferedInputStream(System.in), 
                 new BufferedOutputStream(System.out));
+        //StringUtils.
+        IOUtils.toInputStream("rrr", Charset.defaultCharset());
 
         try        {
             tc.disconnect();
